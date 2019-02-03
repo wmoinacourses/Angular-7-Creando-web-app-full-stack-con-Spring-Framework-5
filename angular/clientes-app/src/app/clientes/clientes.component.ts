68c9239754d414cde9f31c5591f2309f0a898646
Wilmer Moina R <wilmermoinacourses@gmail.com>
2019-02-03 17:18:53 +0100

    Creando archivo clientes.json.ts con la lista de objetos

--- angular/clientes-app/src/app/clientes/clientes.component.ts
+++ angular/clientes-app/src/app/clientes/clientes.component.ts
@@ -1,56 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { CLIENTES } from './clientes.json';
 import { Cliente } from './cliente';
+
 
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html'
 })
 export class ClientesComponent implements OnInit {
-  clientes: Cliente[] = [
-    {
-      id: 1,
-      nombre: 'Wilmer',
-      apellido: 'Moina',
-      email: 'wilmermoinar@gmail.com',
-      createAt: '2017-03-05'
-    },
-    {
-      id: 2,
-      nombre: 'Wilmer',
-      apellido: 'Moina',
-      email: 'wilmermoinar@gmail.com',
-      createAt: '2017-03-05'
-    },
-    {
-      id: 3,
-      nombre: 'Wilmer',
-      apellido: 'Moina',
-      email: 'wilmermoinar@gmail.com',
-      createAt: '2017-03-05'
-    },
-    {
-      id: 4,
-      nombre: 'Wilmer',
-      apellido: 'Moina',
-      email: 'wilmermoinar@gmail.com',
-      createAt: '2017-03-05'
-    },
-    {
-      id: 5,
-      nombre: 'Wilmer',
-      apellido: 'Moina',
-      email: 'wilmermoinar@gmail.com',
-      createAt: '2017-03-05'
-    },
-    {
-      id: 6,
-      nombre: 'Wilmer',
-      apellido: 'Moina',
-      email: 'wilmermoinar@gmail.com',
-      createAt: '2017-03-05'
-    },
-  ];
+
+  clientes: Cliente [];
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.clientes = CLIENTES;
+  }
 }
---
+++ angular/clientes-app/src/app/clientes/clientes.json.ts
@@ -0,0 +1,46 @@
+
+import { Cliente } from './cliente';
+export const CLIENTES: Cliente[] = [
+  {
+    id: 1,
+    nombre: 'Wilmer',
+    apellido: 'Moina',
+    email: 'wilmermoinar@gmail.com',
+    createAt: '2017-03-05'
+  },
+  {
+    id: 2,
+    nombre: 'Wilmer',
+    apellido: 'Moina',
+    email: 'wilmermoinar@gmail.com',
+    createAt: '2017-03-05'
+  },
+  {
+    id: 3,
+    nombre: 'Wilmer',
+    apellido: 'Moina',
+    email: 'wilmermoinar@gmail.com',
+    createAt: '2017-03-05'
+  },
+  {
+    id: 4,
+    nombre: 'Wilmer',
+    apellido: 'Moina',
+    email: 'wilmermoinar@gmail.com',
+    createAt: '2017-03-05'
+  },
+  {
+    id: 5,
+    nombre: 'Wilmer',
+    apellido: 'Moina',
+    email: 'wilmermoinar@gmail.com',
+    createAt: '2017-03-05'
+  },
+  {
+    id: 6,
+    nombre: 'Wilmer',
+    apellido: 'Moina',
+    email: 'wilmermoinar@gmail.com',
+    createAt: '2017-03-05'
+  },
+];
